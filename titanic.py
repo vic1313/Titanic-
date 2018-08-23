740c529db1d148777eafc080dd624da53ffc2e3e
vic1313 <vicspender@yahoo.in>
2018-08-24 01:58:56 +0530

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -135,32 +135,3 @@ df_csv.PassengerId=df_test.PassengerId
 df_csv.Survived=ss
 
 df_csv.to_csv('Titanic_survivor.csv',index=False)
-
-import sklearn
-print (sklearn.metrics.classification_report(Y_test, zz))
-
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import roc_auc_score
-
-
-clf = SVC(kernel='rbf')
-grid_values = {'gamma': [0.001, 0.01, 0.05, 0.1, 1, 10, 100]}
-
-# default metric to optimize over grid parameters: accuracy
-grid_clf_acc = GridSearchCV(clf, param_grid = grid_values)
-grid_clf_acc.fit(X_train, Y_train)
-y_decision_fn_scores_acc = grid_clf_acc.decision_function(X_test) 
-
-print('Grid best parameter (max. accuracy): ', grid_clf_acc.best_params_)
-print('Grid best score (accuracy): ', grid_clf_acc.best_score_)
-
-# alternative metric to optimize over grid parameters: AUC
-grid_clf_auc = GridSearchCV(clf, param_grid = grid_values, scoring = 'roc_auc')
-grid_clf_auc.fit(X_train, Y_train)
-y_decision_fn_scores_auc = grid_clf_auc.decision_function(X_test) 
-
-print('Test set AUC: ', roc_auc_score(Y_test, y_decision_fn_scores_auc))
-print('Grid best parameter (max. AUC): ', grid_clf_auc.best_params_)
-print('Grid best score (AUC): ', grid_clf_auc.best_score_)
-
-
